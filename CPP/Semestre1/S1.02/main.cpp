791d62b965e94f63dc73ea3890c6d199235667a1
Slicksou <etienne.dumai@gmail.com>
2024-01-19 21:18:09 +0100

    publication

---
+++ CPP/Semestre1/S1.02/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file main.cpp
+ * @author Dumai Etienne
+ * @brief Fichier d'execution du jeu de la Bataille Navale
+ * @date 2024-01-08
+ */
+
+#include <iostream>
+#include "game-tools.h"
+#include "bataille-navale.h"
+using namespace std;
+//test
+int main()
+{
+    initialiserLaPartie();
+    jouerLaPartie();
+    finaliserLaPartie();
+    return 0;
+}
+
