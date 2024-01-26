da8b6329f908902c88820fd80cf9383efc14f260
Slicksou <etienne.dumai@gmail.com>
2024-01-26 21:36:43 +0100

    TT fini mais probleme

--- CPP/Perso/RockpaperScissor/main.cpp
+++ CPP/Perso/RockpaperScissor/main.cpp
@@ -14,14 +14,24 @@ enum Signe
     PIERRE = 1,
     CISEAUX = 2
 };
+bool gagnantJ1 = false;
+bool gagnantJ2 = false;
+bool egalite = false;
+string gagnantPartie;
 string pseudoJ1;
 string pseudoJ2;
 int signeJ1;
 int signeJ2;
 void choisirPseudo(string &pseudo1, string &pseudo2);
+void tirerUnSigne(int signe1, int signe2, bool &gagnant1, bool &gagnant2, bool &egaliteJeu);
+string definirGagnant(bool gagnant1, bool gagnant2);
+void afficherGagnant();
 int main(void)
 {
     choisirPseudo(pseudoJ1, pseudoJ2);
+    tirerUnSigne(signeJ1, signeJ2, gagnantJ1, gagnantJ2, egalite);
+    gagnantPartie = definirGagnant(gagnantJ1, gagnantJ2);
+    afficherGagnant();
 }
 
 void choisirPseudo(string &pseudo1, string &pseudo2)
@@ -46,4 +56,83 @@ void choisirPseudo(string &pseudo1, string &pseudo2)
             pause(3);
         }
     }
+}
+
+void tirerUnSigne(int signe1, int signe2, bool &gagnant1, bool &gagnant2, bool &egaliteJeu)
+{
+
+    while (!egaliteJeu)
+    {
+        signe1 = random(0, 2);
+        signe2 = random(0, 2);
+        switch (signe1)
+        {
+        case PAPIER:
+            switch (signe2)
+            {
+            case PAPIER:
+                egaliteJeu = true;
+                break;
+            case PIERRE:
+                gagnant1 = true;
+                break;
+            case CISEAUX:
+                gagnant2 = true;
+            default:
+                break;
+            }
+            break;
+        case PIERRE:
+            switch (signe2)
+            {
+            case PAPIER:
+                gagnant2 = true;
+                break;
+            case PIERRE:
+                egaliteJeu = true;
+                break;
+            case CISEAUX:
+                gagnant1 = true;
+            default:
+                break;
+            }
+            break;
+        case CISEAUX:
+            switch (signe2)
+            {
+            case PAPIER:
+                gagnant1 = true;
+                break;
+            case PIERRE:
+                gagnant2 = true;
+                break;
+            case CISEAUX:
+                egaliteJeu = true;
+            default:
+                break;
+            }
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+string definirGagnant(bool gagnant1, bool gagnant2)
+{
+    string gagnant;
+    if (gagnant1 == true)
+    {
+        gagnant = pseudoJ1;
+    }
+    if (gagnant2 == true)
+    {
+        gagnant = pseudoJ2;
+    }
+    return gagnant;
+}
+
+void afficherGagnant()
+{
+    cout << "Bien jouer " << gagnantPartie << " tu as remporte cette partie" << endl;
 }
