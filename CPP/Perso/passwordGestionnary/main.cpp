d612662bba2beb6cf115c7325ec59ecdb7159339
EtienneDumai <etienne.dumai@gmail.com>
2024-02-05 11:50:21 +0100

    consultation Ok

--- CPP/Perso/passwordGestionnary/main.cpp
+++ CPP/Perso/passwordGestionnary/main.cpp
@@ -19,16 +19,20 @@ char modeDouvertureDuFichier;
 string nomSystemeficSource; // nom système du fichier à consulter
 // DECLARATION DES SOUS PROGRAMMES
 
-void choixModeOuverture(UnModeOuverture& modeOuvertureFichier);
-void modificationDuFichier(UnFichierTexte fic, UnModeOuverture modeOuvertureFichier, string nonSys="fichier.txt");
-    // CORPS DU MAIN
+void choixModeOuverture(UnModeOuverture &modeOuvertureFichier);
+void ouvertureDuFichier(UnFichierTexte &fichiertexte, UnModeOuverture modeOuvertureFichier, string nomSys = "fichier.txt");
+void consultationDuFichier(UnFichierTexte &fic);
+void modificationDuFichier(UnFichierTexte &fic, UnModeOuverture modeouvertureFichier, string nomSys = "fichier.txt");
+// CORPS DU MAIN
 
-    int main()
+int main()
 {
+    choixModeOuverture(modeOuverture);
+    ouvertureDuFichier(fichier, modeOuverture);
     return 0;
 }
 
-void choixModeOuverture(UnModeOuverture& modeOuvertureFichier)
+void choixModeOuverture(UnModeOuverture &modeOuvertureFichier)
 {
     char reponseMode;
     cout << "Quel est votre mode d'ouverture" << endl;
@@ -52,9 +56,46 @@ void choixModeOuverture(UnModeOuverture& modeOuvertureFichier)
     }
 }
 
-void modificationDuFichier(UnFichierTexte fic, UnModeOuverture modeOuvertureFichier, string nonSys)
+void ouvertureDuFichier(UnFichierTexte &fichiertexte, UnModeOuverture modeOuvertureFichier, string nomSys)
 {
-    associer(fic, nonSys);
-    ouvrir(fic, modeOuvertureFichier);
-    
+    associer(fichiertexte, nomSys);
+    ouvrir(fichiertexte, modeOuvertureFichier);
+    if (modeOuvertureFichier == consultation)
+    {
+        consultationDuFichier(fichiertexte);
+    }
+}
+
+void consultationDuFichier(UnFichierTexte &fic)
+{
+    bool trouve = false;
+    bool fdf;
+    string mot;
+    string plateformeDuMotDePasse;
+    cout << "De quelle plateforme sur laquelle vous etes inscrit voulez-vous avoir "
+         << "l'identifiant et le mot de passe ?" << endl;
+    cin >> plateformeDuMotDePasse;
+    while (true)
+    {
+
+        lireMot(fic, mot, fdf);
+        if (mot == plateformeDuMotDePasse)
+        {
+            lireMot(fic, mot, fdf);
+            cout << "Voici l'identifiant : " << mot << endl;
+            lireMot(fic, mot, fdf);
+            cout << "Voici le mot de passe du compte : " << mot << endl;
+            trouve=true;
+            break;
+        }
+        if (fdf == true && trouve==false)
+        {
+            cout << "la plateforme que vous avez saisie n'a pas ete trouve veuillez verifier l'orthographe ou enrgistrer le mot de pass dans l'application" << endl;
+            break;
+        }
+    }
+}
+
+void modificationDuFichier(UnFichierTexte &fic, UnModeOuverture modeouvertureFichier, string nomSys)
+{
 }
