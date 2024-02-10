9c49579203312151281b735d257aa5eeed2ad980
Etienne Dumai <etienne.dumai@gmail.com>
2024-02-10 11:06:46 +0100

    comments

--- Python/perso/test.py
+++ Python/perso/test.py
@@ -2,10 +2,11 @@ import random as r
 def encrypt(message):
     messageEncrypted=''
     for i in message:
-        ord[i]+= r.randint(26,2000)
+        ord[i]+= r.randint(26,2000) # probleme ici jsp pk
         messageEncrypted+=i
     return messageEncrypted
 
 messageAencrypt=''
 messageAencrypt=input('Quel est le message que vous voulez encrypter ?')
 print('Voici votre message encrypté', encrypt(messageAencrypt))
+#probleme ligne 11 jsp pk qui renvoie a la ligne 5
