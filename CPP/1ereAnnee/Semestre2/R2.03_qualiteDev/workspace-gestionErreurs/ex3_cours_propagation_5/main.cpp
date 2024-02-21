909d7dfb6d3dec76baecefa7fe801a92c2330b08
EtienneDumai <etienne.dumai@gmail.com>
2024-02-21 10:09:33 +0100

    ex3 propaga 5 fini

---
+++ CPP/1ereAnnee/Semestre2/R2.03_qualiteDev/workspace-gestionErreurs/ex3_cours_propagation_5/main.cpp
@@ -0,0 +1,82 @@
+/**************************************************************************
+ * PROGRAMME : Propagation exception le long d'unne hiérarchie d'appels
+ * BUT : Codage de l'illustration n°1 du cours
+ * VERSION DE BASE : --
+ * AUTEUR : Dagorret
+ * DATE : 02/02/2022
+ * REMARQUES :
+ *             néant
+ ***************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+/** DECLARATIONS DES sous-programmes
+ *----------------------------------**/
+void sousProg();
+// but : saisie des opérandes et appelle la division
+
+int division(int eltHaut, int eltBas);
+// Calcule et retourne eltHaut/eltBas
+// pré-condition : eltBas  /= 0
+
+/** PROGRAMME PRINCIPAL MAIN
+ *--------------------------**/
+
+int main()
+{
+   cout << "MAIN()_debut " << endl;
+   try
+   {
+      sousProg();
+   }
+   catch (string s)
+   {
+      cout << "Division par zero opération impossible" << endl;
+   }
+
+   cout << "MAIN()_fin" << endl;
+   return 0;
+}
+
+/** DEFINITIONS DES sous-programmes
+ *--------------------------------- **/
+void sousProg()
+{
+   cout << "   SOUSPROG()_debut :" << endl;
+   int x; // dividende
+   int diviseur;
+
+   // saisie des opérandes >> diviseur
+   cout << "   Valeur pour x : ";
+   cin >> x;
+   cout << "   Valeur pour diviseur : ";
+   cin >> diviseur;
+   try
+   {
+      cout << division(x, diviseur);
+   }
+   catch (string)
+   {
+      cerr << "division par 0 " << '\n';
+      throw string("Division par zéro detectee fin du sous prog");
+   }
+
+   // x, diviseur >> calcul et affichage division >> (écran)
+   cout << "   " << x << " / " << diviseur << " = ";
+   cout << "   " << division(x, diviseur) << endl;
+
+   cout << "   SOUSPROG()_fin" << endl;
+}
+
+int division(int eltHaut, int eltBas)
+{
+   if (eltBas == 0)
+   {
+      throw string("calcul impossible, division par zero");
+   }
+   else
+   {
+      return (eltHaut / eltBas);
+   }
+}
