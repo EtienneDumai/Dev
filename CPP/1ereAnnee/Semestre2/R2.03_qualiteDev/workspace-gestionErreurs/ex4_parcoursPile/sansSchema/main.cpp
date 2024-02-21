99dc6d7fb2ee10a1e3c0eaddc75dfc838cfa34d7
EtienneDumai <etienne.dumai@gmail.com>
2024-02-21 10:53:10 +0100

    retour a l'etat initial

--- CPP/1ereAnnee/Semestre2/R2.03_qualiteDev/workspace-gestionErreurs/ex4_parcoursPile/sansSchema/main.cpp
+++ CPP/1ereAnnee/Semestre2/R2.03_qualiteDev/workspace-gestionErreurs/ex4_parcoursPile/sansSchema/main.cpp
@@ -8,45 +8,34 @@
  *             MAIS LE PARCOURS EST ROBUSTE.
  *             En effet, il y utilisation des OBSEVATEURS pour tester
  *             les pré-conditions et NE PAS faire lever les exceptions.
- *
- ***************************************************************************/
+ *             
+***************************************************************************/
 #include <iostream>
 using namespace std;
 #include "pile.h"
 
 int main()
 {
-    // Déclaration et initialisation de la pile
+    //Déclaration et initialisation de la pile
     UnePile maPile;
     initialiser(maPile);
 
-    // Déclaration et peulement d'un tableau
+    // Déclaration et peulement d'un tableau 
     const unsigned int TAILLE = 6;
-    int monTab[TAILLE] = {0, 1, 2, 3, 4, 5};
+    int monTab [TAILLE] = {0, 1, 2, 3, 4, 5};
 
     // maPile, monTab >> remplir pile >> maPile
-    for (unsigned int i = 0; i < TAILLE; i++)
+    for (unsigned int i = 0 ; i < TAILLE; i++)
     {
         empiler(maPile, monTab[i]);
     }
 
     // Affichage du contenu de la pile
-    while (true)
+    while (!estVide(maPile))
     {
-        try
-        {
-            cout << sommet(maPile);
-        }
-        catch (string)
-        {
-            break;
-        }
-
-        cout << "... ";
-        depiler(maPile);
+        cout << sommet(maPile) << "... " ; depiler(maPile);
     }
-    cout << endl
-         << "au revoir..." << endl;
-
+    cout << endl << "au revoir..." << endl;
+   
     return 0;
 }
