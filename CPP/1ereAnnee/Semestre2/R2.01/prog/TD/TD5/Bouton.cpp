c2d98780b72239a2f5bcd9e972133af3d4d34aa2
Etienne Dumai <etienne.dumai@gmail.com>
2024-03-05 12:26:46 +0100

    TD fini

--- CPP/1ereAnnee/Semestre2/R2.01/prog/TD/TD5/Bouton.cpp
+++ CPP/1ereAnnee/Semestre2/R2.01/prog/TD/TD5/Bouton.cpp
@@ -1,65 +1,80 @@
 #include "Bouton.h"
 
-Bouton::Bouton(const Bouton &autre) {
-
+Bouton::Bouton(const Bouton &autre)
+{
+    Cercle(autre);
+    m_estActif(autre.m_estActif);
 }
 
-Bouton::Bouton() {
-
+Bouton::Bouton()
+{
+    Cercle();
+    m_estActif(false);
 }
 
-Bouton::Bouton(const Cercle &c, bool etat) {
-
+Bouton::Bouton(const Cercle &c, bool etat)
+{
+    Cercle(c);
+    m_estActif(etat);
 }
 
-void Bouton::definir(int x, int y, int r, int rouge, int vert, int bleu, bool etat) {
-    
+void Bouton::definir(int x, int y, int rayon, int rouge, int vert, int bleu, bool etat)
+{
+    (*this).placer(x, y);
+    (*this).dimensionner(rayon);
+    (*this).definirCouleur(rouge, vert, bleu);
+    (*this).m_estActif = etat;
 }
 
-void Bouton::definir(int x, int y, int r, const Couleur &couleur, bool etat) {
-
+void Bouton::definir(int x, int y, int rayon, const Couleur &couleur, bool etat)
+{
+    (*this).definir(x, y, rayon, couleur.rouge(), couleur.vert(), couleur.bleu(), etat);
 }
 
-void Bouton::activer(bool etat) {
-
+void Bouton::activer(bool etat)
+{
 }
 
-void Bouton::desactiver(bool etat) {
-
+void Bouton::desactiver(bool etat)
+{
 }
 
-bool Bouton::actif() const {
-
+bool Bouton::actif() const
+{
 }
 
-int Bouton::coordX() const {
-
+int Bouton::coordX() const
+{
 }
 
-int Bouton::coordY() const {
-
+int Bouton::coordY() const
+{
 }
 
-int Bouton::rayon() const {
-
+int Bouton::rayon() const
+{
 }
 
-Couleur Bouton::couleur() const {
-
+Couleur Bouton::couleur() const
+{
 }
 
-void Bouton::definirCouleur(Couleur couleur) {
-
+void Bouton::definirCouleur(Couleur couleur)
+{
 }
 
-void Bouton::afficher(Fenetre &f) const {
-
+void Bouton::afficher(Fenetre &f, Couleur c) const
+{
+    (*this).Cercle::afficher(f);
+    (*this).dessinerEtat(f, (*this).couleur());
 }
 
-void Bouton::effacer(Fenetre &f) const {
-    
+void Bouton::effacer(Fenetre &f, Couleur c) const
+{
+    (*this).Cercle::effacer(f);
+    (*this).dessinerEtat(f, (*this).couleur());
 }
 
-bool Bouton::touche(int x, int y) const {
-    
+bool Bouton::touche(int x, int y) const
+{
 }
--- CPP/1ereAnnee/Semestre2/R2.01/prog/TD/TD5/Bouton.h
+++ CPP/1ereAnnee/Semestre2/R2.01/prog/TD/TD5/Bouton.h
@@ -1,5 +1,5 @@
-#ifndef CERCLE_H
-#define CERCLE_H
+#ifndef BOUTON_H
+#define BOUTON_H
 
 #include <iostream>
 using namespace std;
@@ -37,42 +37,43 @@ class Bouton : public Cercle
 {
 private:
     bool m_estActif;
-    void dessinerEtat(Fenetre &f) const;
+    void dessinerEtat(Fenetre &f, Couleur) const;
+
 public:
-    //constructeur de copie d'un bouton
+    // constructeur de copie d'un bouton
     Bouton(const Bouton &autre);
-    //constructeur par défaut qui créer un bouton dont le cercle est celui par defaut et qui est inactif
+    // constructeur par défaut qui créer un bouton dont le cercle est celui par defaut et qui est inactif
     Bouton();
-    //constructeur permettant de créer un bouton à partir d'un cercle et d'un etat
+    // constructeur permettant de créer un bouton à partir d'un cercle et d'un etat
     Bouton(const Cercle &c, bool etat = false);
 
-    /* methode pour definir sa position, sa taille (rayon du cercle), sa couleur ( trois entiers) et 
+    /* methode pour definir sa position, sa taille (rayon du cercle), sa couleur ( trois entiers) et
     eventuellement son état. Par défaut le bouton sera inactif*/
-    void definir(int x, int y, int r, int rouge, int vert, int bleu, bool etat = false);
-    /*methode pour definir sa position, sa taille (rayon du cercle), sa couleur (objet de la classe Couleur) 
+    void definir(int x, int y, int rayon, int rouge, int vert, int bleu, bool etat = false);
+    /*methode pour definir sa position, sa taille (rayon du cercle), sa couleur (objet de la classe Couleur)
     puis son etat. Par défaut le bouton sera bleu et inactif*/
-    void definir(int x, int y, int r, const Couleur &couleur = Couleur(0, 0, 255), bool etat = false);
-    //methode activer qui met le dans l'etat actif
+    void definir(int x, int y, int rayon, const Couleur &couleur = Couleur(0, 0, 255), bool etat = false);
+    // methode activer qui met le dans l'etat actif
     void activer(bool etat = true);
-    //methode desactiver qui met le bouton dans l'etat inactif
+    // methode desactiver qui met le bouton dans l'etat inactif
     void desactiver(bool etat = false);
-    //methode actif qui retourne true si le bouton est actif, false sinon
+    // methode actif qui retourne true si le bouton est actif, false sinon
     bool actif() const;
-    //methode qui retourne la coordonnée x du bouton
+    // methode qui retourne la coordonnée x du bouton
     int coordX() const;
-    //methode qui retourne la coordonnée y du bouton
+    // methode qui retourne la coordonnée y du bouton
     int coordY() const;
-    //methode qui retourne le rayon du bouton
+    // methode qui retourne le rayon du bouton
     int rayon() const;
-    //methode couleur qui retourne la couleur du bouton
+    // methode couleur qui retourne la couleur du bouton
     Couleur couleur() const;
-    //methode definirCouleur qui permet de definir la couleur du bouton à partir de la classe Couleur
+    // methode definirCouleur qui permet de definir la couleur du bouton à partir de la classe Couleur
     void definirCouleur(Couleur couleur);
-    //methode afficher qui permet d'afficher le bouton dans la fenetre passée en paramètre
+    // methode afficher qui permet d'afficher le bouton dans la fenetre passée en paramètre
     void afficher(Fenetre &f) const;
-    //methode effacer qui permet d'effacer le bouton de la fenetre passée en paramètre
+    // methode effacer qui permet d'effacer le bouton de la fenetre passée en paramètre
     void effacer(Fenetre &f) const;
-    //methode touche qui retourne true si les coordonnées passées en paramètres sont celles d'un point situé à l'intérieur du bouton, false sinon
+    // methode touche qui retourne true si les coordonnées passées en paramètres sont celles d'un point situé à l'intérieur du bouton, false sinon
     bool touche(int x, int y) const;
 };
 
