ab3f562cc3e0dbc67e771e6c89370c6cab39595c
EtienneDumai <etienne.dumai@gmail.com>
2024-03-06 15:25:14 +0100

    erreurs mais en cours

---
+++ CPP/1ereAnnee/Semestre2/R2.03_qualiteDev/Nodenot/TP1/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string Substring(const string chaine, int Deb, int Longueur);
+string chaineTest;
+int debut;
+int longueur;
+int main()
+{
+    cout << "Entrez la valeur de la chaine : ";
+    cin >> chaineTest;
+    cout << "Entrez la valeur de Deb sous la forme d'un entier :";
+    cin >> debut;
+    cout << "Entrez la valeur de Longueur sous la forme d'un entier :";
+    cin >> longueur;
+    try
+    {
+        Substring(chaineTest, debut, longueur);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    string Substring(const string chaine, int Deb, int Longueur)
+    {
+        string chaineCoupe;
+        
+        try
+        {
+            if (Deb < 0)
+            {
+                throw string("Exception : Deb ne peut pas etre negatif");
+            }
+        }
+        catch (string s)
+        {
+            cerr << s;
+        }
+        return 0;
+    }
+
+    return string();
+}
