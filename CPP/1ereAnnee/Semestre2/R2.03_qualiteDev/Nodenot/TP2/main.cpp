27d769a2487ca148d52abd7223bfecb94b4aaa92
Etienne Dumai <etienne.dumai@gmail.com>
2024-03-20 14:58:38 +0100

    main fini

--- CPP/1ereAnnee/Semestre2/R2.03_qualiteDev/Nodenot/TP2/main.cpp
+++ CPP/1ereAnnee/Semestre2/R2.03_qualiteDev/Nodenot/TP2/main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
+using namespace std;
 
-unsigned int maxSum(int value, int maxint);
+int maxSum(int value, int maxint);
 
 int main(void)
 {
-    
+    int valValue;
+    int valMaxInt;
+    int tabValue[] = {7, -5, 5, 2, 2};
+    const int NBTESTS = 5;
+    int tabMaxInt[] = {30, 20, 10, 1, 4};
+    int results[] = {28, 15, -1, -1, -1};
+    int cpterreurs = 0;
+    for (int i = 0; i < NBTESTS; i++)
+    {
+        int valValue = tabValue[i];
+        int valMaxInt = tabMaxInt[i];
+        int resultAutoMaxsum = results[i];
+        int resultobtenu = maxSum(valValue, valMaxInt);
+        if (!(resultobtenu == resultAutoMaxsum))
+            cpterreurs++;
+        cout << "value" + to_string(valValue) + "maxInt=" + to_string(valMaxInt) + " : resultat attendu par Oracle etait :" + to_string(resultAutoMaxsum) + " resultat obtenu=" + to_string(resultobtenu) << endl;
+    }
+    cout<<"NbTests effectues: "+ to_string(NBTESTS) + " et Nbtests failed :"+ to_string(cpterreurs);
     return 0;
 }
 
-unsigned int maxSum(int value, int maxint){
-
+int maxSum(int value, int maxint){
+    
 }
